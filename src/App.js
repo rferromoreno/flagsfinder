4ff67bf5f17d7a804958cb2a9cc8cf8290ccfc73
Ricardo <rferromoreno@gmail.com>
2017-10-06 11:54:52 -0300

    Borré comentarios obsoletos

--- src/App.js
+++ src/App.js
@@ -27,38 +27,3 @@ class App extends Component {
 
 export default App;
 
-/*
-class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { 
-      playing: false,
-      room: null
-    }
-    this.setGame=this.setGame.bind(this);
-    this.props.socket.on('game:created:ok', (payload) => {
-      this.setState({
-        playing: true,
-        room: payload.room    
-      });
-    });
-  }
-
-  setGame() {
-    this.props.socket.emit('game:create', this.props.socket.id);
-  }
-
-  render() {
-    return (
-      <div className="App">
-      {
-        this.state.playing ?
-         <Game room={this.state.room}/> :
-         <Chat setGame={this.setGame}/>
-      }
-      </div>
-    );
-  }
-}
-*/
